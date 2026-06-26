41038c6733e7848db1b923a4c82c186a7f286c05
Gayatri Chhetri <gayatrichhetri@gmail.com>
2026-06-26 17:05:41 +0545

    update backend port

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 ConnectDB()
   .then(() => {
-    app.listen(9000, () => {
+    app.listen(process.env.PORT || 9000, () => {
       console.log("Server is running");
     });
   })
