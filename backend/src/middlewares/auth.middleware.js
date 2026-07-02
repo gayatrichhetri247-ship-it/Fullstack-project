e6f7bbd22ae7c6d91be439b4a834cb05b14f2766
Gayatri Chhetri <gayatrichhetri@gmail.com>
2026-07-02 09:28:31 +0545

    fixing token problem

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,45 +1,42 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import userModel from "../models/user.model.js";
 
-export const varifyToken= async(req,res,next)=>{
-   export const varifyToken = async (req, res, next) => {
-    console.log("Cookies:", req.cookies);
-    console.log("Cookie Header:", req.headers.cookie);
+export const varifyToken = async (req, res, next) => {
+  console.log("Cookies:", req.cookies);
+  console.log("Cookie Header:", req.headers.cookie);
 
-    const token = req.cookies?.token;
+  const token = req.cookies?.token;
 
-    if (!token) {
-        return res.status(400).json({
-            message: "token not provided",
-            success: false,
-        });
-    }
-
-    // ...
-}
-
-    try{
-         const encoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-         const user=await userModel.findById(encoded.id);
-         req.user = user; 
-         next();
-    }
-    catch(error){
-        res.status(400).json({
-            message:"Invalid Token",
-            success:false
-        })
-    }
-
-};
-export const isAdmin = (req,res,next)=>{
-   const user = req.user;
-
-   if(user.role!=="admin"){
-    return res.status(403).json({
-        message:"Admin only",
-        success:false
+  if (!token) {
+    return res.status(400).json({
+      message: "token not provided",
+      success: false,
     });
-   }
-   next();
-}
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await userModel.findById(decoded.id);
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      message: "Invalid Token",
+      success: false,
+    });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({
+      message: "Admin only",
+      success: false,
+    });
+  }
+
+  next();
+};
