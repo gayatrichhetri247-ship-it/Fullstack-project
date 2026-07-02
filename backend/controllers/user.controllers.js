28b8442f385d3841d3abafae7af28e64a5dbd828
Gayatri Chhetri <gayatrichhetri@gmail.com>
2026-07-02 08:52:12 +0545

    fixing cookies issues

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -3,50 +3,53 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
-  try{
+  try {
     const { fullname, email, password } = req.body;
 
-  const existingUser = await userModel.findOne({ email });
+    const existingUser = await userModel.findOne({ email });
 
-  if (existingUser) {
-    return res.status(400).json({
-      message: "User already exist with this email",
-      success: false,
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exist with this email",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+      fullname,
+      email,
+      password: hashedPassword,
     });
-  }
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "1h",
+      },
+    );
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
 
-  const user = await userModel.create({
-    fullname,
-    email,
-    password: hashedPassword,
-  });
-  const token = jwt.sign(
-    {
-      id: user._id,
-    },
-    process.env.JWT_SECRET_KEY,
-    {
-      expiresIn: "1h",
-    },
-  );
-
-  res.cookie("token", token);
-
-  res.status(201).json({
-    message: "User Siggned up",
-    success: true,
-    user,
-  });
-
-  }catch(error){
-     res.status(500).json({
+    res.status(201).json({
+      message: "User Siggned up",
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
       message: "Internal server error",
       success: false,
       error,
     });
-
   }
 };
 
@@ -95,48 +98,44 @@ export const LoginUser = async (req, res) => {
   }
 };
 
-export const LogoutUser = (req,res)=>{
-    try{
-        res.clearCookie("token");
-        res.status(200).json({
-            message:"User Logged out successfully",
-            success:true
-        })
-
-    }
-    catch (error) {
+export const LogoutUser = (req, res) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({
+      message: "User Logged out successfully",
+      success: true,
+    });
+  } catch (error) {
     res.status(500).json({
       message: "Internal server error",
       success: false,
       error,
     });
   }
-}
+};
 
-export const getMe = async(req,res)=>{
-    try{
-        const user = req.user;
-        if(!user){
-            return res.status(404).json({
-                message:"user not found",
-                success:false
-            })
-        }
-        res.status(200).json({
-            message:"user fetched successfully",
-            success:true,
-            user
-        });
-
+export const getMe = async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+        success: false,
+      });
     }
-    catch (error) {
+    res.status(200).json({
+      message: "user fetched successfully",
+      success: true,
+      user,
+    });
+  } catch (error) {
     res.status(500).json({
       message: "Internal server error",
       success: false,
       error,
     });
   }
-}
+};
 export const getUsers = async (req, res) => {
   try {
     const users = await userModel.find();
